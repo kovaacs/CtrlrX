bd9f0ce1f21a7f2c26d945687d353f6d76b22d8e
Damien S <55164385+damiensellier@users.noreply.github.com>
2025-01-26 22:56:34 -0500

    5.6.31 CtrlrRevision.h updated with ctrlrRevision variable taken directly from the projucer project version field

--- Source/Core/CtrlrRevision.h
+++ Source/Core/CtrlrRevision.h
@@ -1,7 +1,11 @@
 #ifndef __CTRLR_REVISION__
 #define __CTRLR_REVISION__
 
-static const char *ctrlrRevision              	= "5.6.31";
+
+
+// static const char *ctrlrRevision              	= "5.6.31";
 // static const char *ctrlrRevisionDate        	= "Thu, 23 Jan 2025, 14:00:00 EST";
-static const char *ctrlrRevisionDate            = __TIMESTAMP__; // C++ macro to show timestamp. Fri Jan 24 16:00:00 2025
+
+static const char *ctrlrRevision                = JucePlugin_VersionString; // Updated v5.6.31. version taken from Projucer project settings
+static const char *ctrlrRevisionDate            = __TIMESTAMP__; // Updated v5.6.31. C++ macro to show timestamp. Fri Jan 24 16:00:00 2025
 #endif
